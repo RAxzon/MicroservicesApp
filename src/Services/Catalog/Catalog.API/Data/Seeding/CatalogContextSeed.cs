da84e5ff2222727e10d1e1a8788ba8caa45da3a8
Rikard Axelsson <Richiehstad@gmail.com>
2021-04-07 16:20:50 +0200

    Implemented connection to mongoDb and added repository for product the class

---
+++ src/Services/Catalog/Catalog.API/Data/Seeding/CatalogContextSeed.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data.Seeding
+{
+    public static class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            // Check if the db contains any records
+            var doesDbExist = productCollection.Find(p => true).Any();
+
+            if (!doesDbExist)
+            {
+                productCollection.InsertManyAsync(GetPreconfiguredProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetPreconfiguredProducts()
+        {
+            return new List<Product>
+            {
+                new Product()
+                {
+                    Id = "602d2149e773f2a399047f5",
+                    Name = "Samsung Galaxy S20",
+                    Summary = "Summary of Samsung Galaxy S20",
+                    Description = "Description of Samsung Galaxy S20",
+                    ImageFile = "product2.png",
+                    Price = 750.00M,
+                    Category = "SmartPhone"
+                },
+                new Product()
+                {
+                    Id = "602d2149e773f2a399047f6",
+                    Name = "Samsung Galaxy S21",
+                    Summary = "Summary of Samsung Galaxy S21",
+                    Description = "Description of Samsung Galaxy S21",
+                    ImageFile = "product1.png",
+                    Price = 950.00M,
+                    Category = "SmartPhone"
+                },
+                new Product()
+                {
+                    Id = "602d2149e773f2a399047f7",
+                    Name = "PlayStation 4",
+                    Summary = "Summary of PlayStation 4",
+                    Description = "Description of PlayStation 4",
+                    ImageFile = "product3.png",
+                    Price = 3500.00M,
+                    Category = "Console"
+                },
+                new Product()
+                {
+                    Id = "602d2149e773f2a399047f8",
+                    Name = "PlayStation 5",
+                    Summary = "Summary of PlayStation 5",
+                    Description = "Description of PlayStation 5",
+                    ImageFile = "product4.png",
+                    Price = 5500.00M,
+                    Category = "Console"
+                },
+            };
+        }
+    }
+}
