d5f8f2a112b3da21a4009e4684727db6e67100bf
Rikard Axelsson <Richiehstad@gmail.com>
2021-04-20 09:15:21 +0200

    Implemented entities, repository and controller for the basket microservice.

---
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Basket.API.Entities;
+using Basket.API.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Basket.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[Controller]")]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _basketRepository;
+        private readonly ILogger _logger;
+
+        public BasketController(IBasketRepository basketRepository, ILogger logger)
+        {
+            _basketRepository = basketRepository ?? throw new ArgumentException(nameof(basketRepository));
+            _logger = logger ?? throw new ArgumentException(nameof(logger));
+        }
+
+        [HttpGet("{userName}", Name = "GetBasket")]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+        {
+            try
+            {
+                var basket = await _basketRepository.GetBasket(userName);
+
+                return Ok(basket ?? new ShoppingCart(userName));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error getting shopping cart with username: {userName}", ex);
+                throw new Exception($"Error getting basket with username: {userName}", ex);
+            }
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(ShoppingCart), (int) HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody]ShoppingCart basket)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    return Ok(await _basketRepository.UpdateBasket(basket));
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError($"Error updating the basket with username: {basket.UserName}", ex);
+                    throw new Exception($"Error updating the basket with username: {basket.UserName}", ex);
+                }
+            }
+
+            return BadRequest(basket);
+        }
+
+        [HttpDelete]
+        [ProducesResponseType(typeof(void), (int) HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteBasket(string userName)
+        {
+            try
+            {
+                await _basketRepository.DeleteBasket(userName);
+                return Ok(userName);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error deleting the basket with username: {userName}", ex);
+                throw new Exception($"Error deleting the basket with username: {userName}", ex);
+            }
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Basket.API.Entities;
+using Basket.API.Repositories.Interfaces;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+
+        private readonly IDistributedCache _redis;
+
+        public BasketRepository(IDistributedCache redis)
+        {
+            _redis = redis ?? throw new ArgumentException(nameof(redis));
+        }
+
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await _redis.GetStringAsync(userName);
+            return string.IsNullOrEmpty(basket) ? null : JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            await _redis.SetStringAsync(basket.UserName, JsonConvert.SerializeObject(basket));
+            return await GetBasket(basket.UserName);
+        }
+
+        public async Task DeleteBasket(string UserName)
+        {
+            await _redis.RemoveAsync(UserName);
+        }
+
+    }
+}
