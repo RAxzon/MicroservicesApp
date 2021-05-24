bacc2617ab89419f7d1476eeeb9d976ec44a208f
Rikard Axelsson <Richiehstad@gmail.com>
2021-05-24 13:59:58 +0200

    Added gRPC support for the Dscount microservice

---
+++ src/Services/Discount/Discount.Grpc/Extension/HostExtensions.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+
+namespace Discount.Grpc.Extension
+{
+    public static class HostExtensions
+    {
+        public static IHost MigrateDatabase<TContext>(this IHost host, int? retry = 0)
+        {
+
+            var retryConnection = 0;
+
+            if (retry != null)
+            {
+                retryConnection = retry.Value;
+            }
+
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            var configuration = services.GetRequiredService<IConfiguration>();
+            var logger = services.GetRequiredService<ILogger<TContext>>();
+
+            try
+            {
+                logger.LogInformation($"Migrating database");
+
+                using var connection = new NpgsqlConnection(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+                connection.Open();
+
+                using var command = new NpgsqlCommand
+                {
+                    CommandText = "DROP TABLE IF EXISTS Coupon", Connection = connection
+                };
+
+                Seed(command);
+
+                logger.LogInformation("Successfully migrated to database");
+            }
+            catch (NpgsqlException ex)
+            {
+                logger.LogError(ex, "An error occurred when migrating to db");
+
+                if (retryConnection < 50)
+                {
+                    retryConnection++;
+                    System.Threading.Thread.Sleep(2000);
+                    MigrateDatabase<TContext>(host, retryConnection);
+                }
+            }
+
+            return host;
+        }
+
+        private static void Seed(NpgsqlCommand command)
+        {
+            command.ExecuteNonQuery();
+
+            command.CommandText = @"CREATE TABLE Coupon(Id SERIAL PRIMARY KEY, ProductName VARCHAR(24) NOT NULL, Description TEXT, Amount INT)";
+            command.ExecuteNonQuery();
+
+            command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('Playstation 4', 'Playstation 4 discount', 500)";
+            command.ExecuteNonQuery();
+
+            command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('Playstation 5', 'Playstation 5 discount', 400)";
+            command.ExecuteNonQuery();
+        }
+    }
+}
---
+++ src/Services/Discount/Discount.Grpc/Repositories/DiscountRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dapper;
+using Discount.Grpc.Entities;
+using Discount.Grpc.Repositories.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+
+namespace Discount.Grpc.Repositories
+{
+    public class DiscountRepository : IDiscountRepository
+    {
+
+        private readonly IConfiguration _configuration;
+        private readonly NpgsqlConnection _connection;
+
+        public DiscountRepository(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+            _connection = new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+        }
+
+        public async Task<IEnumerable<Coupon>> GetDiscounts()
+        {
+            var coupons = await _connection.QueryAsync<Coupon>("SELECT * FROM Coupon");
+            return coupons;
+        }
+
+        public async Task<Coupon> GetDiscount(string productName)
+        {
+            var coupon = await _connection.QueryFirstOrDefaultAsync<Coupon>("SELECT * FROM Coupon WHERE ProductName = @ProductName", productName);
+            return coupon;
+        }
+
+        public async Task<bool> CreateDiscount(Coupon coupon)
+        {
+            var insert = await _connection.ExecuteAsync(
+                "INSERT INTO Coupon (ProductName, Description, Amount) VALUES (@ProductName, @Description, @Amount)",
+                new {coupon.ProductName, coupon.Description, coupon.Amount
+        });
+
+            return insert != 0;
+        }
+
+        public async Task<bool> UpdateDiscount(Coupon coupon)
+        {
+            var updated = await _connection.ExecuteAsync(
+                "UPDATE Coupon SET ProductName=@ProductName, Description=@Description, Amount=@Amount WHERE ProductName = @productName", 
+                new {coupon.ProductName, coupon.Description, coupon.Amount});
+
+            return updated != 0;
+        }
+
+        public async Task<bool> DeleteDiscount(string productName)
+        {
+            var deleted = await _connection.ExecuteAsync("DELETE FROM Coupon WHERE productName = @ProductName", productName);
+
+            return deleted != 0;
+        }
+
+    }
+}
