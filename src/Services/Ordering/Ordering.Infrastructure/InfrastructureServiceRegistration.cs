280616e3a1c2b3cf4724ccf59641aeadc8dba0ed
Rikard Axelsson <Richiehstad@gmail.com>
2021-08-10 14:01:33 +0200

    Added classes for service registration.

---
+++ src/Services/Ordering/Ordering.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Contracts.Infrastructure;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Models;
+using Ordering.Infrastructure.Mail;
+using Ordering.Infrastructure.Persistence;
+using Ordering.Infrastructure.Repositories;
+using Ordering.Infrastructure.Repositories.Order;
+
+namespace Ordering.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<OrderContext>(opt => opt
+                .UseSqlServer(config.GetConnectionString("OrderConnectionString")));
+            
+            // Makes an instance every time a request comes in
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+            services.AddScoped<IOrderRepository, OrderRepository>();
+
+            // Creates an instance only once when the application starts
+            services.Configure<EmailSettings>(c => config.GetSection("EmailSettings"));
+            services.AddTransient<IEmailService, EmailService>();
+
+            return services;
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Mail/EmailService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Ordering.Application.Contracts.Infrastructure;
+using Ordering.Application.Models;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace Ordering.Infrastructure.Mail
+{
+    public class EmailService : IEmailService
+    {
+        private readonly ILogger<EmailService> _logger;
+        public EmailSettings EmailSettings { get; }
+
+        public EmailService(ILogger<EmailService> logger, IOptions<EmailSettings> emailSettings)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            EmailSettings = emailSettings.Value;
+        }
+
+        public async Task<bool> SendEmail(Email email)
+        {
+            var client = new SendGridClient(EmailSettings.ApiKey);
+            var subject = email.Subject;
+            var to = new EmailAddress(email.To);
+            var body = email.Body;
+
+            var from = new EmailAddress()
+            {
+                Email = EmailSettings.FromAddress,
+                Name = EmailSettings.FromName
+            };
+
+            var sendGridMessage = MailHelper.CreateSingleEmail(from, to, subject, body, body);
+            var response = await client.SendEmailAsync(sendGridMessage);
+
+            _logger.LogInformation("Email sent");
+
+            return true;
+        }
+    }
+}
